eb9627c940be224d50212bb8e6cf1aa66f0a6d91
StudingJin <kimjin5605@naver.com>
2025-04-27 20:36:17 +0900

    fix(storage): Force image upload to PNG format

--- services/hw_services/storage_service.py
+++ services/hw_services/storage_service.py
@@ -5,12 +5,14 @@ async def upload_image_to_firebase(image_file) -> str:
     """
     이미지 파일을 firebase Storage에 업로드하고 공개 URL로 변환
     """
-    filename = f"images/{uuid.uuid4()}.jpg"
+    filename = f"images/{uuid.uuid4()}.png"
     blob = bucket.blob(filename)
 
     # 이미지 파일의 내용을 읽어서 업로드
-    blob.upload_from_file(image_file.file, 
-                        content_type=image_file.content_type)
+    blob.upload_from_file(
+        image_file.file,
+        content_type="image/png"
+    )
 
     # URL 접근 허용
     blob.make_public()
