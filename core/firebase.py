3d452758b17829237bb2999d75ab0e3aa1c1a3b0
SeungeunKang <120700820+user00144@users.noreply.github.com>
2025-05-03 09:48:58 +0900

    Update firebase.py

--- core/firebase.py
+++ core/firebase.py
@@ -1,16 +1,14 @@
 import os
 import firebase_admin
-import json
 from firebase_admin import credentials, firestore, storage
 
 #인증 키 경로 불러오기
 cred_path = os.getenv("FIREBASE_CREDENTIAL_PATH")
 bucket_name = os.getenv("FIREBASE_BUCKET_NAME")
-cred_dict = json.loads(cred_path.replace("\n","\\n"))
 
 #앱 초기화 (한번만)
 if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_dict)
+    cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred, {
         "storageBucket": os.getenv("FIREBASE_BUCKET_NAME")
 
