fc0f9f569bb396b02b7d2e2bf274530c035b91fc
SeungeunKang <120700820+user00144@users.noreply.github.com>
2025-05-13 17:54:45 +0900

    Revert "Feature/client/get calendar info"

--- services/client_services/preview_image_service.py
+++ services/client_services/preview_image_service.py
@@ -1,23 +1,35 @@
-from core.firebase_utils import get_contents_ref
-from fastapi import HTTPException
+from core.firebase import db
+from typing import List
 
 async def get_preview_images(device_id: str, date: str) -> dict:
     """"
-    id_list 기반으로(날짜) 컨텐츠 이미지 url 가져오기 (이중 리스트 형태로 반환)
+    id_list 기반으로 컨텐츠 이미지 url 가져오기 (이중 리스트 형태로 반환)
     """
 
-    contents_ref = get_contents_ref(device_id)
+    contents_ref = db.collection("devices").document(device_id).collection("contents")
     docs = contents_ref.stream()
 
-    ids, urls = zip(*[
-        (doc.id, data.get("image_url", ""))
-        for doc in docs
-        if (data := doc.to_dict()).get("created_at") and data.get("created_at").split("T")[0] == date
-    ]) if docs else ([], [])
+    ids = []
+    urls = []
 
-    if not ids:
-        raise HTTPException(status_code=404, detail="해당 날짜에 컨텐츠가 없습니다.")
-    
+    for doc in docs:
+        data = doc.to_dict()
+        created_at = data.get("created_at")
+
+        if not created_at:
+            continue
+
+        content_date = created_at.split("T")[0]
+
+        if content_date != date:
+            continue
+
+        ids.append(doc.id)
+        urls.append(data.get("image_url", ""))
+        
     return {
-        "contents_list_image": [list(ids,), list(urls)]
+        "contents_list_image": [ids, urls]
     }
+
+
+
