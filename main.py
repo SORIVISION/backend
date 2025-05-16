d35425901fb9b2824bdd57eea0d94151ae72d5d7
CommonLion <tndud4327@naver.com>
2025-05-16 09:14:03 +0900

    refactor(main.py): Update comments and clean up code structure

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from api.v1 import v1
 
-app = FastAPI()
-
-# "/ap1/v1" 아래 모든 라우터 등록
+## "/ap1/v1" 아래 모든 라우터 등록
 app.include_router(v1, prefix="/api/v1")
 
 if __name__ == "__main__" :
